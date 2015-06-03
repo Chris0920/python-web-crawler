8f5d07cba6aa520921f5de1a15634578a42aa105
Chris920 <cdtded1212@gmail.com>
2015-06-02 22:45:57 -0400

    test

--- crawler.py
+++ crawler.py
@@ -18,7 +18,7 @@ def get_next_target(page):
         return None, 0
     start_quote = page.find('"', start_link)
     end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1:end_quote]
+    url = page[start_quote + 1: end_quote]
     return url, end_quote
 
 def union(p,q):
